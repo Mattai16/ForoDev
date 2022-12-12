ee992c6efe343dd870bc640ed569b117a8d8a3cc
Matt Martinez <martinezmonteromateo@gmail.com>
2022-12-12 14:41:10 -0600

    Corección

--- post.js
+++ post.js
@@ -3,14 +3,16 @@ const posts = JSON.parse(localStorage.getItem('posts')) || [];
 const render = () => {
     const postList = document.getElementById('post-list');
 
-    const postsTemplate = posts.map(t => '<li>' + t + '</li>' + '<form id="res-form">' + '<input type="text" id="res">' +
+/*     const postsTemplate = posts.map(t => '<li>' + t + '</li>' + '<form id="res-form">' + '<input type="text" id="res">' +
     '<input type="submit">' + '</form>');
+ */
+    const postsTemplate = posts.map(t => '<li>' + t + '</li>');
     console.log(postsTemplate);
 
     postList.innerHTML = postsTemplate.join('');
 
     const elementos = document.querySelectorAll('#post-list li');
-
+/* 
     elementos.forEach((elemento, i) => {
         elemento.addEventListener('click', () => {
             elemento.parentNode.removeChild(elemento);
@@ -18,7 +20,7 @@ const render = () => {
             actualizaT(posts);
             render();
         })
-    });
+    }); */
 }
 
 const actualizaT = (posts) => {
